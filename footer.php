2b25d9e07557eeaded7f6f8d2c2ba81c2a6c126c
Martin SION <martin@sionfamily.com>
2021-05-19 00:32:40 +0200

    Work footer

--- footer.php
+++ footer.php
@@ -1,25 +1,38 @@
-	<!-- Footer -->
-	<footer>
-		<!-- Réseaux sociaux -->
-		<section>
-			<a href="https://fb.me/lsa360" title="facebook ls-a"><img src="img/socials/fb 3.svg" alt="logo Facebook"></a>
-			<a href="https://instagram.com/lsa360" title="instagram ls-a"><img src="img/socials/inst 3.svg" alt="logo Instagram"></a>
-			<a href="https://linkedin.com/company/ls-a" title="linkedin ls-a"><img src="img/socials/twi 3.svg" alt="logo Linkedin"></a>
-		</section>
-		<!-- Adresse -->
-		<section>
-			<p>Société ls-a</p>
-			<p>53 rue de la Paix</p>
-			<p>10000 Troyes</p>
-		</section>
-		<!-- Mentions légales -->
-		<section>
-			<span>Copyright 2021 ls-a - </span>
-			<a href="https://ls-a.fr/mentions-legales">Mentions légales</a>
-		</section>
-	</footer>
-	
-	<!-- JS global -->
-	<script src="js/script.js"></script>
-</body>
+		<!-- Footer -->
+		<footer>
+			<div class="footer1">
+				<!-- Réseaux sociaux -->
+				<section>
+					<a href="" title="facebook ls-a"><img src="img/socials/fb 3.svg" alt="logo Facebook"></a>
+					<a href="" title="instagram ls-a"><img src="img/socials/inst 3.svg" alt="logo Instagram"></a>
+					<a href="" title="linkedin ls-a"><img src="img/socials/twi 3.svg" alt="logo Linkedin"></a>
+				</section>
+				<!-- Payment -->
+				<section>
+					<a href="" title="facebook ls-a"><img src="img/socials/visa.svg" alt="logo Facebook"></a>
+					<a href="" title="instagram ls-a"><img src="img/socials/mastercard.svg" alt="logo Instagram"></a>
+					<a href="" title="linkedin ls-a"><img src="img/socials/paypal.svg" alt="logo Linkedin"></a>
+					<a href="" title="linkedin ls-a"><img src="img/socials/applepay.svg" alt="logo Linkedin"></a>
+				</section>
+			</div>
+
+			<div class="footer2">
+
+			</div>
+			<!-- Adresse -->
+			<section>
+				<p>Société ls-a</p>
+				<p>53 rue de la Paix</p>
+				<p>10000 Troyes</p>
+			</section>
+			<!-- Mentions légales -->
+			<section>
+				<span>Copyright 2021 ls-a - </span>
+				<a href="https://ls-a.fr/mentions-legales">Mentions légales</a>
+			</section>
+		</footer>
+		
+		<!-- JS global -->
+		<script src="js/script.js"></script>
+	</body>
 </html>

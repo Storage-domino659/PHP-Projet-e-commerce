f71adb8bffed5e93e7c9c32ec53ed1504634d5c7
Martin SION <martin@sionfamily.com>
2021-05-19 02:10:34 +0200

    Still in footer

--- footer.php
+++ footer.php
@@ -9,27 +9,51 @@
 				</section>
 				<!-- Payment -->
 				<section>
-					<a href="" title="facebook ls-a"><img src="img/socials/visa.svg" alt="logo Facebook"></a>
-					<a href="" title="instagram ls-a"><img src="img/socials/mastercard.svg" alt="logo Instagram"></a>
-					<a href="" title="linkedin ls-a"><img src="img/socials/paypal.svg" alt="logo Linkedin"></a>
-					<a href="" title="linkedin ls-a"><img src="img/socials/applepay.svg" alt="logo Linkedin"></a>
+					<img src="img/socials/visa.svg" alt="logo Visa">
+					<img src="img/socials/mastercard.svg" alt="logo MasterCard">
+					<img src="img/socials/paypal.svg" alt="logo Paypal">
+					<img src="img/socials/applepay.svg" alt="logo Applepay">
 				</section>
 			</div>
 
 			<div class="footer2">
 
-			</div>
-			<!-- Adresse -->
-			<section>
-				<p>Société ls-a</p>
-				<p>53 rue de la Paix</p>
-				<p>10000 Troyes</p>
-			</section>
-			<!-- Mentions légales -->
-			<section>
-				<span>Copyright 2021 ls-a - </span>
-				<a href="https://ls-a.fr/mentions-legales">Mentions légales</a>
-			</section>
+	
+				<!-- Help And Information -->
+				<section>
+					<h2>HELP AND INFORMATION</h2>
+					<br>
+					<h3>Help</h3>
+					<h3>Track Order</h3>
+					<h3>Delivery & Returns</h3>
+					<h3>Premier Delivery</h3>
+					<h3>Privacy & Cookies</h3>
+				</section>
+				<!-- About -->
+				<section>
+					<h2>ABOUT</h2>
+					<br>
+					<h3>About us</h3>
+					<h3>Careers</h3>
+					<h3>Corporate Responsability</h3>
+					<h3>Investors Site</h3>
+					<h3>Ts&Cs</h3>
+					<h3>Accessibility</h3>
+				</section>
+				<!-- Shopping From -->
+				<section>
+					<h2>SHOPPING FROM:</h2>
+					<br>
+					<h3>France</h3>
+				</section>
+				<!-- Logo/Login -->
+				<section>
+					<h2>HELP AND INFORMATION</h2>
+					<br>
+					<h3>53 rue de la h3aix</h3>
+					<h3>10000 Troyes</h3>
+				</section> 
+
 		</footer>
 		
 		<!-- JS global -->
